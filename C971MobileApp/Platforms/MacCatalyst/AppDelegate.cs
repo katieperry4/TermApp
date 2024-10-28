903e7117a52e622997c546a40bc38b2475e41b19
katieperry4 <katelynnperry99@gmail.com>
2024-10-28 14:03:03 -0400

    Created MAUI app

---
+++ C971MobileApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace C971MobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
