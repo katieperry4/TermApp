d4e6f2d294bff9b30e161fc8348298e03e6c139a
katieperry4 <katelynnperry99@gmail.com>
2024-11-01 09:42:26 -0400

    adding DB seed data

--- C971MobileApp/C971MobileApp/DBService.cs
+++ C971MobileApp/C971MobileApp/DBService.cs
@@ -21,6 +21,37 @@ namespace C971MobileApp
             _connection.CreateTableAsync<Assessment>();
         }
 
+        public async Task SeedData()
+        {
+            var termCount = await _connection.Table<Term>().CountAsync();
+            if (termCount == 0)
+            {
+                await _connection.InsertAsync(new Term { TermName = "Term 1", TermStart = new DateTime(2024, 9, 1), TermEnd = new DateTime(2025, 2, 28) });
+            }
+
+            var courseCount = await _connection.Table<Course>().CountAsync();
+            if (courseCount == 0)
+            {
+                var term = await _connection.Table<Term>().FirstOrDefaultAsync();
+                if (term != null)
+                {
+                    await _connection.InsertAsync(new Course {CourseName = "Hardware and Operating Systems", CourseStatus = "In Progress", CIName = "Anika Patel", CIEmail = "anika.patel@strimeuniversity.com", CIPhone = "555-123-4567", CourseStart = new DateTime(2024, 10,15), CourseEnd = new DateTime(2024,11,30), CourseNotes = "This is a very difficult class, it's a good thing I have this notes section to write down all of my thoughts.", StartNotif = false, EndNotif = false, TermId=term.TermId });
+                }
+            }
+
+            var assessmentCount = await _connection.Table<Assessment>().CountAsync();
+            if (assessmentCount == 0) 
+            {
+                var course = await _connection.Table<Course>().FirstOrDefaultAsync();
+                if (course != null) 
+                {
+                    await _connection.InsertAsync(new Assessment {AssessmentName = "Hardware PA", AssessmentType = "Performance", CourseId = course.CourseID, AssessmentStart = new DateTime(2024, 11, 8), AssessmentEnd = new DateTime(2024, 11, 15), StartNotif = false, EndNotif = false});
+                    await _connection.InsertAsync(new Assessment {AssessmentName = "Proctored Test", AssessmentType = "Objective", CourseId = course.CourseID, AssessmentStart = new DateTime(2024, 11, 20), AssessmentEnd = new DateTime(2024, 11,21), StartNotif = false, EndNotif = false });
+                }
+            }
+
+        }
+
         public async Task<List<Term>> GetAllTerms()
         {
             return await _connection.Table<Term>().ToListAsync();
--- C971MobileApp/C971MobileApp/obj/Debug/net8.0-android/C971MobileApp.AssemblyInfo.cs
+++ C971MobileApp/C971MobileApp/obj/Debug/net8.0-android/C971MobileApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C971MobileApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a414fa713fd844923ada06e8743e362735275246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1ea6113fd7471f594783b109800bd94778478c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("C971MobileApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C971MobileApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
